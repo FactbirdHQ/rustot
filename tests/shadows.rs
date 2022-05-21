8d571fefbf0eb5a6f7b9f0e00adb749f6da2ef85
Mathias <mk@blackbird.online>
2022-05-21 13:50:06 +0200

    Fix statemachine of shadows test

--- tests/shadows.rs
+++ tests/shadows.rs
@@ -1,18 +1,17 @@
 mod common;
 
 use common::{clock::SysClock, credentials, network::Network};
-use heapless::String;
-use mqttrust::Mqtt;
-use mqttrust_core::{bbqueue::BBBuffer, EventLoop, MqttOptions, Notification, PublishNotification};
+use mqttrust_core::{bbqueue::BBBuffer, EventLoop, MqttOptions, Notification};
 use native_tls::TlsConnector;
-use rustot::shadows::{derive::ShadowState, Shadow, ShadowState};
+use rustot::shadows::{derive::ShadowState, Shadow};
 use serde::{Deserialize, Serialize};
 
 use smlang::statemachine;
 
-static mut Q: BBBuffer<{ 1024 * 6 }> = BBBuffer::new();
+const Q_SIZE: usize = 1024 * 6;
+static mut Q: BBBuffer<Q_SIZE> = BBBuffer::new();
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
 pub enum SensorType {
     Npn,
@@ -26,15 +25,108 @@ impl Default for SensorType {
     }
 }
 
-statemachine! {
-    transitions: {
-        *Init + Subscribed = Ready,
+impl SensorType {
+    pub fn next_type(&self) -> Self {
+        match self {
+            Self::Npn => Self::Pnp,
+            Self::Pnp => Self::Analog,
+            Self::Analog => Self::Npn,
+        }
     }
 }
 
-pub struct TestContext {}
+statemachine! {
+    transitions: {
+        *Begin + Run / begin = Update(SensorType),
+        Update(SensorType) + CheckState / check = Check(Option<SensorType>),
+        Check(Option<SensorType>) + UpdateState / update = Update(SensorType),
+    }
+}
 
-impl StateMachineContext for TestContext {}
+impl core::fmt::Debug for States {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Begin => write!(f, "Self::Begin"),
+            Self::Update(t) => write!(f, "Self::Update({:?})", t),
+            Self::Check(t) => write!(f, "Self::Check({:?})", t),
+        }
+    }
+}
+
+pub struct TestContext<'a> {
+    shadow: Shadow<'a, SensorConf, mqttrust_core::Client<'static, 'static, Q_SIZE>>,
+}
+
+impl<'a> StateMachineContext for TestContext<'a> {
+    fn check(&mut self, sensor_type: &SensorType) -> Option<SensorType> {
+        if matches!(sensor_type, SensorType::Analog) {
+            None
+        } else {
+            Some(*sensor_type)
+        }
+    }
+
+    fn update(&mut self, sensor_type: &Option<SensorType>) -> SensorType {
+        sensor_type.unwrap().next_type()
+    }
+
+    fn begin(&mut self) -> SensorType {
+        SensorType::Npn
+    }
+}
+
+impl<'a> StateMachine<TestContext<'a>> {
+    pub fn spin(&mut self, notification: Notification) -> bool {
+        log::info!("State: {:?}", self.state());
+        match (self.state(), notification) {
+            (&States::Update(desired_type), Notification::Publish(_)) => {
+                self.context_mut()
+                    .shadow
+                    .update(|_current, desired| {
+                        desired.sensor_type = Some(desired_type);
+                    })
+                    .unwrap();
+                self.process_event(Events::CheckState).unwrap();
+            }
+            (&States::Check(expected_type), Notification::Publish(publish))
+                if matches!(
+                    publish.topic_name.as_str(),
+                    "$aws/things/rustot-test/shadow/update/accepted"
+                ) =>
+            {
+                log::info!(
+                    "{:?}: {:?}",
+                    publish.topic_name,
+                    core::str::from_utf8(&publish.payload)
+                );
+                self.context_mut()
+                    .shadow
+                    .handle_message(&publish.topic_name, &publish.payload)
+                    .unwrap();
+
+                match expected_type {
+                    Some(expected_type) => {
+                        assert_eq!(self.context_mut().shadow.get().sensor_type, expected_type);
+                        self.context_mut().shadow.get_shadow().unwrap();
+                        self.process_event(Events::UpdateState).unwrap();
+                    }
+                    None => {
+                        return true;
+                    }
+                }
+            }
+            (_, Notification::Publish(publish)) => {
+                self.context_mut()
+                    .shadow
+                    .handle_message(&publish.topic_name, &publish.payload)
+                    .unwrap();
+            }
+            _ => {}
+        }
+
+        false
+    }
+}
 
 #[derive(Debug, Default, Serialize, ShadowState)]
 pub struct SensorConf {
@@ -68,45 +160,20 @@ fn test_shadows() {
 
     let mqtt_client = mqttrust_core::Client::new(p, thing_name);
 
-    let mut sensor_conf_handler = Shadow::new(SensorConf::default(), &mqtt_client).unwrap();
-
-    let mut test_state = StateMachine::new(TestContext {});
+    let mut test_state = StateMachine::new(TestContext {
+        shadow: Shadow::new(SensorConf::default(), &mqtt_client).unwrap(),
+    });
 
     loop {
-        match mqtt_eventloop.connect(&mut network) {
-            Ok(true) => {
-                log::info!("Successfully connected to broker");
-                sensor_conf_handler.get_shadow().unwrap();
-            }
-            Ok(false) => {}
-            Err(nb::Error::WouldBlock) => continue,
-            Err(e) => panic!("{:?}", e),
+        if nb::block!(mqtt_eventloop.connect(&mut network)).expect("to connect to mqtt") {
+            log::info!("Successfully connected to broker");
+            test_state.context_mut().shadow.get_shadow().unwrap();
+            test_state.process_event(Events::Run).unwrap();
         }
 
-        match mqtt_eventloop.yield_event(&mut network) {
-            Ok(Notification::Suback(_)) => {
-                log::info!("Suback");
-                test_state.process_event(Events::Subscribed);
-            }
-            Ok(Notification::Publish(mut publish)) => {
-                log::info!(
-                    "received: {:?}",
-                    core::str::from_utf8(&publish.payload).ok()
-                );
-                if let Some(update_state) = sensor_conf_handler
-                    .handle_message(&publish.topic_name, &publish.payload)
-                    .unwrap()
-                {
-                    log::info!(
-                        "Setting sensor to {:?} from the cloud",
-                        update_state.sensor_type
-                    );
-                }
-            }
-            Ok(n) => {
-                log::trace!("{:?}", n);
-            }
-            _ => {}
+        let notification = nb::block!(mqtt_eventloop.yield_event(&mut network)).unwrap();
+        if test_state.spin(notification) {
+            break;
         }
     }
 }
