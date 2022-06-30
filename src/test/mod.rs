4702a14a7b513dbbf730a96bdbcd72d1be2c1317
Mathias <mk@blackbird.online>
2022-06-30 09:14:23 +0200

    Change from fugit-timer to embedded-hal countdown timers for OTA request timer, to allow utilizing IRQ's

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,4 +1,7 @@
-use std::{cell::RefCell, collections::VecDeque};
+use std::{
+    cell::{Cell, RefCell},
+    collections::VecDeque,
+};
 
 use mqttrust::{encoding::v4::encode_slice, Mqtt, MqttError, Packet};
 
@@ -7,24 +10,27 @@ use mqttrust::{encoding::v4::encode_slice, Mqtt, MqttError, Packet};
 ///
 pub struct MockMqtt {
     pub tx: RefCell<VecDeque<Vec<u8>>>,
-    publish_fail: bool,
+    publish_fail: Cell<bool>,
 }
 
 impl MockMqtt {
     pub fn new() -> Self {
         Self {
             tx: RefCell::new(VecDeque::new()),
-            publish_fail: false,
+            publish_fail: Cell::new(false),
         }
     }
 
-    pub fn publish_fail(&mut self) {
-        self.publish_fail = true;
+    pub fn publish_fail(&self) {
+        self.publish_fail.set(true);
     }
 }
 
 impl Mqtt for MockMqtt {
     fn send(&self, packet: Packet<'_>) -> Result<(), MqttError> {
+        if self.publish_fail.get() {
+            return Err(MqttError::Full);
+        }
         let v = &mut [0u8; 1024];
 
         let len = encode_slice(&packet, v).map_err(|_| MqttError::Full)?;
