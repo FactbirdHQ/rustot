0ac39a78b30b171d72dabaa85b70491b70025f5d
Mathias <mk@blackbird.online>
2023-09-12 14:20:43 +0200

    Update pfx credentials for integration test to new format used by openssl3

--- scripts/register.sh
+++ scripts/register.sh
@@ -16,7 +16,10 @@ SECRETS_DIR=$(dirname $0)/../tests/secrets
 
 PUBLIC_KEY=$(openssl ec -in $SECRETS_DIR/sign_public.pem -pubin -text -noout 2>/dev/null | awk 'NR>2 && NR<8 {gsub(/ /,""); gsub(/:/, ""); print $0}' | tr -d '\n' | awk '{ gsub(/\r/, ""); print substr($0,3)}' | xxd -r -p | base64 -w0 -)
 
-aws lambda invoke --function-name "ms-device-provisioner-register" --output text --cli-binary-format raw-in-base64-out --payload '{"uuid": "rustot-provision", "public_key": "'$PUBLIC_KEY'", "device_type": "fbduo", "hardware_version": "test"}' response.json >/dev/null
+API_KEY_ID="wxghvqsvp8"
+API_KEY=`aws apigateway get-api-key --api-key $API_KEY_ID --include-value --query value --output text`
+
+curl -X POST -L -H "Content-Type: application/json" -H "x-api-key: $API_KEY" -d '{"uuid": "rustot-provision", "public_key": "'$PUBLIC_KEY'", "device_type": "fbduo", "hardware_version": "test"}' register.blackbird.online -o response.json
 
 # Store files to `$SECRETS_DIR/claim_certificate.pem.crt` & `$SECRETS_DIR/claim_private.pem.key`
 jq -r '.certificateId' response.json > $SECRETS_DIR/claim_certificate.id
