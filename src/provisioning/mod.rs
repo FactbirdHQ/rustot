04ac157fff48b362aeb5b302b69d27a1594d0aef
Kenneth Knudsen <98805797+KennethKnudsen97@users.noreply.github.com>
2024-07-19 13:24:54 +0200

    Fix: Async shadow (#60)
    * fix asyunc shadow
    
    * renaming of handle message and some linting
    
    * shadows error fix and handle delta should wait for connected
    
    * fmt

--- src/provisioning/mod.rs
+++ src/provisioning/mod.rs
@@ -138,9 +138,7 @@ impl FleetProvisioner {
         C: DeserializeOwned,
     {
         use crate::provisioning::data_types::CreateCertificateFromCsrResponse;
-
         let mut create_subscription = Self::begin(mqtt, csr, payload_format).await?;
-
         let mut message = create_subscription
             .next()
             .await
