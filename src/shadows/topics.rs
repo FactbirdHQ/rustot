4091e7ef03bece0a248ad3c7445c2c6382c31cd4
Mathias <mk@factbird.com>
2026-02-02 14:15:13 +0100

    Loosen static requirement on topic str

--- src/shadows/topics.rs
+++ src/shadows/topics.rs
@@ -97,7 +97,7 @@ impl Topic {
         &self,
         prefix: &str,
         thing_name: &str,
-        shadow_name: Option<&'static str>,
+        shadow_name: Option<&str>,
     ) -> Result<String<L>, Error> {
         let (name_prefix, shadow_name) = shadow_name.map(|n| ("/name/", n)).unwrap_or_default();
 
@@ -249,7 +249,7 @@ impl<const N: usize> Subscribe<N> {
         self,
         prefix: &str,
         thing_name: &str,
-        shadow_name: Option<&'static str>,
+        shadow_name: Option<&str>,
     ) -> Result<heapless::Vec<(heapless::String<128>, QoS), N>, Error> {
         assert!(thing_name.len() <= MAX_THING_NAME_LEN);
 
@@ -289,7 +289,7 @@ impl<const N: usize> Unsubscribe<N> {
         self,
         prefix: &str,
         thing_name: &str,
-        shadow_name: Option<&'static str>,
+        shadow_name: Option<&str>,
     ) -> Result<heapless::Vec<heapless::String<256>, N>, Error> {
         assert!(thing_name.len() <= MAX_THING_NAME_LEN);
 
